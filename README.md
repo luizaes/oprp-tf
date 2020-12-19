820cba7f738d63d7925bd5f5cb30e7eada4c082a
Luiza <luiza.engler@vtex.com.br>
2020-12-19 17:39:07 -0300

    Adiciona informações no README e código sequencial

--- README.md
+++ README.md
@@ -1,14 +1,37 @@
-### Trabalho Final de OPRP
+## Trabalho Final de Programação Paralela (OPRP)
 
-##### Relatório do Checkpoint 1
-* Nessa primeira fase do trabalho focamos no entendimento do código base e no estudo de como poderíamos melhorá-lo, levando em consideração otimizações, paralelização e algoritmos para solucionar os problemas em questão. Inicialmente tentamos retirar as recursões do código, mas isso só foi possível para a função de posicionamento dos cavalos. A partir daí buscamos soluções para paralelizar o posicionamento das rainhas, que é o processo mais custoso. Chegamos a algumas ideias:
-    * Manter a recursividade e paralelizar as chamadas recursivas limitando a criação de threads até uma certa profundidade da árvore de recursão, para evitar o estouro devido ao excesso de threads criadas;
-    * Modificar o algoritmo para posicionamento de rainhas usando outra lógica, como uma heurística. Um exemplo seria utilizar um algoritmo genético para minimizar o número de conflitos entre as rainhas, paralelizando alguns processos dentro das gerações, como as funções de crossover e fitness.
-* Nos dois casos estamos estudando utilizar a biblioteca OpenMP ou MPI para realizar a paralelização, mas não decidimos ainda de forma definitiva. O código fonte anexado possui as modificações até então feitas, que envolvem a adaptação do posicionamento de cavalos sem recursão e a modificação do posicionamento das rainhas, que possui um bracktracking (garante a solução ótima) mais otimizado. Não temos certeza se esse backtracking será realmente o utilizado, pois por mais que garanta a solução ótima o tempo de execução é muito grande para casos onde n >= 30.
+### Compilação e execução
+```
+make clean
 
-##### Relatório do Checkpoint 2
-* *Escrever de 5 a 10 linhas sobre o desenvolvimento, desafios e ideias*
+make
 
-##### Links úteis
-* [Paralelização de backtracking](https://www.drdobbs.com/architecture-and-design/three-parallel-backtracking-designs/232300302)
-* [Solução com Min-conflicts](https://medium.com/@carlosgonzalez_39141/using-ai-to-solve-the-n-queens-problem-2a5a9cc5c84c)
+time ./knights-queens <número  de  linhas/colunas  do  board>  <número  de  cavalos>
+```
+
+### Problema
+
+> O problema N Queens and K knights envolve posicionar K cavalos em um
+> tabuleiro M x M e, além disso, posicionar a maior quantidade de
+> rainhas possíveis nas células restantes, de modo que nenhum cavalo ou
+> rainha esteja sob ataque.
+
+### Arquitetura da Solução
+
+- A lógica da solução desenvolvida é a seguinte: primeiro a rotina de posicionamento de cavalos é executada, e em seguida, quando todos os K cavalos foram inseridos no tabuleiro, executamos o posicionamento do maior número de rainhas possível.
+	- Para posicionar os cavalos é executada uma estratégia gulosa, passando apenas uma vez por todas as células do tabuleiro e inserindo o cavalo caso seja possível.
+	- Já para as rainhas, a estratégia foi:
+		- Utilizar um backtracking que recursivamente procura todas as soluções válidas e salva a melhor, assim sempre retornando a solução ótima;
+		- Implementar uma técnica de pruning para cortar caminhos da árvore de recursão que não são interessantes de se explorar (ou seja, que não conseguem nos trazer uma solução melhor do que a que já foi encontrada até o momento);
+		- E paralelizar a função de posicionamento das rainhas, utilizando a biblioteca OpenMP.
+- A paralelização foi feita baseada na diretiva de tasks. Tasks são blocos/seções de código que são criados para serem executados em algum momento por uma thread disponível. A ideia é que usamos a diretiva parallel para criar um pool de threads; a diretiva single para que apenas uma thread chame a função `queens()` e seja responsável por dar o empurrão inicial para começar a explorar a árvore de soluções; e a diretiva task, que cria um bloco de código contendo a nossa chamada recursiva como uma tarefa e adiciona ela automaticamente a uma "fila" para ser executada por alguma thread da pool criada.
+- Como sabemos que as árvores de recursão de backtracking para um problema complexo, com tantas possíveis soluções como o das rainhas, é gigante, adicionamos uma lógica de limitação de criação dessas tasks: toda vez que fazemos uma chamada recursiva, verificamos em que profundidade dessa árvore estamos, baseado na coluna em qual estamos tentando alocar a rainha. Caso estivermos dentro do limite de profundidade definido pela constante `LEVEL`, criamos uma task para ela. Caso contrário, não criamos.
+
+### Comparação
+
+- Uma versão sequencial do mesmo código paralelo está presente no arquivo seq.cpp. Para testes e comparações de tempo e speedup deve-se compilar e executar a mesma como abaixo:
+```
+g++ seq.cpp -o sequencial -std=c++14
+
+time ./sequencial <número  de  linhas/colunas  do  board>  <número  de  cavalos>
+```
---
+++ seq.cpp
@@ -0,0 +1,372 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace std;
+
+/* Define variáveis globais */
+int m, n, k, lin, col;
+int count_queens = 0;
+char **final_board;
+
+/* Preenche o tabuleiro */
+void makeBoard(char** board)
+{
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			board[i][j] = '_';
+		}
+	}
+}
+
+/* Funções para printar o tabuleiro */
+void displayBoard(char** board)
+{
+	cout << endl;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << "\t" << board[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void displayBoardCoordenatesMatrix(char** board)
+{
+	cout << endl;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if ((board[i][j] != 'K') && (board[i][j] != 'Q')) {
+				cout << "\t" << (i*m)+(j+1) << ";" ;
+			} else {
+				cout << "\t" << board[i][j] << ";" ;
+			}
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void displayResult(char** board)
+{
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if (board[i][j] == 'K') {
+				cout << "K" << (i*m)+(j+1) << ";" ;
+			}
+			if (board[i][j] == 'Q') {
+				cout << "Q" << (i*m)+(j+1) << ";" ;
+			}
+		}
+	}
+	cout << endl;
+}
+
+void displayBoardCoordenates(char** board)
+{
+	cout << endl;
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if ((board[i][j] == 'K') || (board[i][j] == 'Q')) {
+				cout << (i*m)+(j+1) << ";" ;
+			}
+		}
+	}
+	cout << endl;
+}
+
+/* 
+	Função responsável por marcar as posições de 
+	ataque de um cavalo no tabuleiro
+*/
+void attack(int i, int j, char a, char** board)
+{
+
+	if ((i + 2) < m && (j - 1) >= 0) {
+		board[i + 2][j - 1] = a;
+	}
+	if ((i - 2) >= 0 && (j - 1) >= 0) {
+		board[i - 2][j - 1] = a;
+	}
+	if ((i + 2) < m && (j + 1) < n) {
+		board[i + 2][j + 1] = a;
+	}
+	if ((i - 2) >= 0 && (j + 1) < n) {
+		board[i - 2][j + 1] = a;
+	}
+	if ((i + 1) < m && (j + 2) < n) {
+		board[i + 1][j + 2] = a;
+	}
+	if ((i - 1) >= 0 && (j + 2) < n) {
+		board[i - 1][j + 2] = a;
+	}
+	if ((i + 1) < m && (j - 2) >= 0) {
+		board[i + 1][j - 2] = a;
+	}
+	if ((i - 1) >= 0 && (j - 2) >= 0) {
+		board[i - 1][j - 2] = a;
+	}
+}
+
+/* 
+	Função que verifica se é possível posicionar uma 
+	rainha na célula especificada
+*/
+int attackqueens(int oi, int oj, char** board)
+{
+	int i,j;
+
+	i = oi;
+	j = oj;
+	// Olha todas as posições na diagonal cima/esquerda
+	while ((i > 0) && (j > 0)) {
+		if (board[i-1][j-1] == 'K' || board[i-1][j-1] == 'Q') {
+			return 1;
+		}
+		i--;
+		j--;
+	}
+
+	i = oi;
+	j = oj;
+	// Olha todas as posições em cima
+	while (i > 0) {
+		if (board[i-1][j] == 'Q' || board[i-1][j] == 'K') {
+			
+			return 1;
+		}
+		i--;
+	}
+
+	i = oi;
+	j = oj;
+	// Olha todas as posições na diagonal cima/direita
+	while ((i > 0) && (j < n)) {
+		if (board[i-1][j+1] == 'Q' || board[i-1][j+1] == 'K') {
+			return 1;
+		}
+		i--;
+		j++;
+	}
+
+	i = oi;
+	j = oj;
+	// Olha todas as posições na esquerda
+	while (j > 0) {
+		if (board[i][j-1] == 'Q' || board[i][j-1] == 'K') {
+			return 1;
+		}
+		j--;
+	}
+
+	i = oi;
+	j = oj;
+	// Olha todas as posições na direita
+	while (j+1 < n) {
+		if (board[i][j+1] == 'Q' || board[i][j+1] == 'K') {
+			return 1;
+		}
+		j++;
+	}
+
+	i = oi;
+	j = oj;
+	// Olha todas as posições na diagonal baixo/esquerda
+	while ((i+1 < m) && (j > 0)) {
+		if (board[i+1][j-1] == 'Q' || board[i+1][j-1] == 'K') {
+			return 1;
+		}
+		i++;
+		j--;
+	}
+
+	i = oi;
+	j = oj;
+	// Olha todas as posições pra baixo
+	while (i+1 < m) {
+		if (board[i+1][j] == 'Q' || board[i+1][j] == 'K') {
+			return 1;
+		}
+		i++;
+	}
+
+	i = oi;
+	j = oj;
+	// Olha todas as posições na diagonal baixo/direita
+	while ((i+1 < m) && (j+1 < n)) {
+		if (board[i+1][j+1] == 'Q' || board[i+1][j+1] == 'K') {
+			return(1);
+		}
+		i++;
+		j++;
+	}
+
+	return 0;
+}
+
+/* Verificar se a posição no tabuleiro está disponível */
+bool canPlace(int i, int j, char** board)
+{
+	if (board[i][j] == '_')
+		return true;
+	else
+		return false;
+}
+
+/* 
+	Para cavalos, posiciona eles no tabuleiro e marca seus ataques
+	Para rainhas, verifica se é possível posicioná-las
+*/
+int place(int i, int j, char k, char a, char** board)
+{
+	if (k == 'K') {
+		board[i][j] = k;
+		attack(i, j, a, board);
+	} else {
+		return(attackqueens(i, j, board));
+	}
+	return 0;
+}
+
+/*
+	Função que verifica se um caminho da árvore de recursão
+	já chegou em uma solução válida, e salva ela caso seja
+	a melhor encontrada até então
+*/
+int foundSolution(int quj, char** board, int local_queens) {
+	if(quj == m) {
+		
+		if(local_queens > count_queens) {
+			count_queens = local_queens;
+
+			for(int i = 0; i < m; i++) {
+				for(int j = 0; j < n; j++) {
+					final_board[i][j] = board[i][j];
+				}
+			}
+		}
+		return 1;
+	}
+
+	return 0;
+}
+
+/*
+	Função que cuida do posicionamento das rainhas
+*/
+void queens (int qui, int quj, char ** board, int qtd_rainhas) {
+
+	if((qtd_rainhas + m - quj) <= count_queens)
+		return;
+
+	if(foundSolution(quj, board, qtd_rainhas))
+		return;
+
+	for(int i = qui; i < n; i++) {
+		if (canPlace(i, quj, board)) {
+			if (!place(i, quj, 'Q', 'a', board)) {
+				board[i][quj] = 'Q';
+	      		queens(lin, quj+1, board, qtd_rainhas+1);
+	      		board[i][quj] = '_';
+			}
+		}
+	}
+
+  	queens(lin, quj+1, board, qtd_rainhas);
+}
+
+/* 
+	Função responsável por posicionar os k cavalos
+	no tabuleiro
+*/
+void kkn(int k, char** board)
+{
+
+	int max = k/m;
+	int total_max = (k%m > 0) ? max+1 : max;
+	bool stop = false;
+
+	if(total_max + (total_max-1)*2 > m){
+		cout << "It's not possible to position this number of knights in the board" << endl;
+		exit(1);
+	}
+
+	lin = col = 0;
+
+	for (int i = 0; i < m && !stop; i++) {
+		for (int j = 0; j < n && !stop; j++) {
+			if(k == 0) {
+				stop = true;
+			} else {
+				if (canPlace(i, j, board)) {
+					place(i, j, 'K', 'A', board);
+					if(j >= col) {
+						col = j+1;
+						lin = i+1;
+					}
+					k--;
+				}
+			}
+		}
+	}
+
+	if(k == 0) {
+		for(int x = 0; x < m; x++) {
+			for(int y = 0; y < n; y++) {
+				final_board[x][y] = board[x][y];
+			}
+		}
+		
+		queens(lin, col, board, 0);
+
+		cout << endl << "Final board:" << endl;
+		displayBoardCoordenatesMatrix(final_board);
+		cout << "Positions:" << endl;
+		displayResult(final_board);
+
+		cout << endl << "All the knights were placed" << endl;
+		cout<< "Total number of queens placed: " << count_queens << endl;
+
+		for (int x = 0; x < m; x++) {
+			delete[] board[x];
+		}
+		delete[] board;
+
+		for (int x = 0; x < m; x++) {
+			delete[] final_board[x];
+		}
+		delete[] final_board;
+
+		exit(1);
+	}
+}
+
+/* Função main */
+int main( int argc, char *argv[])
+{
+	if (argc <= 2 ) {
+		printf("./sequencial <linha_tabuleiro> <nro_cavalos>\n");
+		return -1;
+	}
+
+	m = atoi(argv[1]);
+	n = m;
+	k = atoi(argv[2]);
+
+	// Cria os tabuleiros
+	char** board = new char*[m];
+	for (int i = 0; i < m; i++) {
+		board[i] = new char[n];
+	}
+
+	final_board = new char*[m];
+	for (int i = 0; i < m; i++) {
+		final_board[i] = new char[n];
+	}	
+
+	makeBoard(board);
+
+	kkn(k, board);
+
+	return 0;
+}
